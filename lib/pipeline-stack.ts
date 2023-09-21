db3ec8c03ce35d37993449faaea4f8252d42a9bc
Evan Diewald <evandiewald@gmail.com>
2023-09-21 15:46:04 -0400

    try workflow

---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import { GitHubWorkflow, AwsCredentials } from 'cdk-pipelines-github';
+import { ShellStep } from "aws-cdk-lib/pipelines";
+import { Construct } from 'constructs';
+
+export class PipelineStack extends cdk.Stack {
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const githubActionRoleArn = ssm.StringParameter.fromStringParameterName(this, 'githubActionRoleArn', 'cdk-github-action-role-arn');
+
+        const pipeline = new GitHubWorkflow(this, 'Pipeline', {
+            synth: new ShellStep('Build', {
+                commands: [
+                    'npm ci',
+                    'npm run build:runtime',
+                    'npm run build',
+                    'npm run cdk synth',
+                    'npm run lint',
+                    'pip3 install -r runtime/requirements.txt',
+                    'npm run test',
+                ],
+            }),
+            awsCreds: AwsCredentials.fromOpenIdConnect({
+                gitHubActionRoleArn: githubActionRoleArn.stringValue,
+            }),
+        });
+
+    }
+
+}
+
+
