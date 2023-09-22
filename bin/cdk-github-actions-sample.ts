b79679df677f56a9338f43bbb6190365e2fb5f3b
Evan Diewald <evandiewald@gmail.com>
2023-09-22 09:36:32 -0400

    try again

--- bin/cdk-github-actions-sample.ts
+++ bin/cdk-github-actions-sample.ts
@@ -2,7 +2,16 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from "../lib/pipeline-stack";
+import {PipelineAppStage} from "../lib/pipeline-app-stage";
+
 
 const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack', {});
+const stack = new PipelineStack(app, 'PipelineStack', {});
+stack.pipeline.addStage(new PipelineAppStage(app, 'devStage', {
+    env: {
+        account: '352937523354',
+        region: 'us-east-1',
+    },
+}));
 
+app.synth();
--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,5 +1,4 @@
 import * as cdk from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import { GitHubWorkflow, AwsCredentials } from 'cdk-pipelines-github';
 import { ShellStep } from "aws-cdk-lib/pipelines";
@@ -7,14 +6,14 @@ import { Construct } from 'constructs';
 import { PipelineAppStage } from "./pipeline-app-stage";
 
 export class PipelineStack extends cdk.Stack {
+    public pipeline: GitHubWorkflow;
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const githubActionRoleArn = ssm.StringParameter.fromStringParameterName(this, 'githubActionRoleArn', 'cdk-github-action-role-arn');
-        const developmentAccountId = ssm.StringParameter.fromStringParameterName(this, 'developmentAccountId', 'development-account-id');
 
-        const pipeline = new GitHubWorkflow(this, 'Pipeline', {
+        this.pipeline = new GitHubWorkflow(this, 'Pipeline', {
             synth: new ShellStep('Build', {
                 commands: [
                     'npm ci',
@@ -23,19 +22,13 @@ export class PipelineStack extends cdk.Stack {
                 ],
             }),
             awsCreds: AwsCredentials.fromOpenIdConnect({
-                gitHubActionRoleArn: githubActionRoleArn.stringValue,
+                gitHubActionRoleArn: '${{ secrets.GITHUB_ACTIONS_ROLE_ARN }}',
             }),
             preBuildSteps: [
                 { uses: 'actions/setup-node@v3', with: { nodeVersion: process.version }},
             ],
         });
 
-        pipeline.addStage(new PipelineAppStage(this, 'devStage', {
-            env: {
-                account: '352937523354',
-                region: 'us-east-1',
-            },
-        }));
     }
 
 }
