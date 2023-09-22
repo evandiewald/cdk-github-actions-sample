b20590e89b7e2c8dce3d0368ce63c243416a034f
Evan Diewald <evandiewald@gmail.com>
2023-09-22 13:44:53 -0400

    fix

--- lib/cdk-github-actions-sample-stack.ts
+++ lib/cdk-github-actions-sample-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import path from "path";
+import * as path from 'path';
 
 export class CdkGithubActionsSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
