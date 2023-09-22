cda9138f91e56e025df7b8a5fe3c320676a0a441
Evan Diewald <evandiewald@gmail.com>
2023-09-22 13:21:42 -0400

    wrong secret

--- bin/cdk-github-actions-sample.ts
+++ bin/cdk-github-actions-sample.ts
@@ -3,15 +3,12 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from "../lib/pipeline-stack";
 import {PipelineAppStage} from "../lib/pipeline-app-stage";
+import {CdkGithubActionsSampleStack} from "../lib/cdk-github-actions-sample-stack";
 
 
 const app = new cdk.App();
-const stack = new PipelineStack(app, 'PipelineStack', {});
-stack.pipeline.addStage(new PipelineAppStage(app, 'devStage', {
-    env: {
-        account: '352937523354',
-        region: 'us-east-1',
-    },
-}));
+
+// const devAccountId = process.env.DEVELOPER_ACCOUNT_ID;
+const stack = new CdkGithubActionsSampleStack(app, 'CdkGithubActionsSampleStack', {});
 
 app.synth();
--- lib/pipeline-app-stage.ts
+++ lib/pipeline-app-stage.ts
@@ -3,7 +3,6 @@ import { Construct } from "constructs";
 import { CdkGithubActionsSampleStack } from "./cdk-github-actions-sample-stack";
 
 export class PipelineAppStage extends cdk.Stage {
-
     constructor(scope: Construct, id: string, props?: cdk.StageProps) {
         super(scope, id, props);
 
