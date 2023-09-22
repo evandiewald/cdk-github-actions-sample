a4766a94c8eca2de37b189d39e8100f6d3325370
Evan Diewald <evandiewald@gmail.com>
2023-09-22 09:41:44 -0400

    wrong secret

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,9 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
 import { GitHubWorkflow, AwsCredentials } from 'cdk-pipelines-github';
 import { ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from 'constructs';
-import { PipelineAppStage } from "./pipeline-app-stage";
 
 export class PipelineStack extends cdk.Stack {
     public pipeline: GitHubWorkflow;
@@ -11,7 +9,7 @@ export class PipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const githubActionRoleArn = ssm.StringParameter.fromStringParameterName(this, 'githubActionRoleArn', 'cdk-github-action-role-arn');
+        // const githubActionRoleArn = ssm.StringParameter.fromStringParameterName(this, 'githubActionRoleArn', 'cdk-github-action-role-arn');
 
         this.pipeline = new GitHubWorkflow(this, 'Pipeline', {
             synth: new ShellStep('Build', {
@@ -22,7 +20,7 @@ export class PipelineStack extends cdk.Stack {
                 ],
             }),
             awsCreds: AwsCredentials.fromOpenIdConnect({
-                gitHubActionRoleArn: '${{ secrets.GITHUB_ACTIONS_ROLE_ARN }}',
+                gitHubActionRoleArn: '${{ secrets.ACTIONS_ROLE_ARN }}',
             }),
             preBuildSteps: [
                 { uses: 'actions/setup-node@v3', with: { nodeVersion: process.version }},
