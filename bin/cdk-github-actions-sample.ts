5c374cd537045eabf3feb7a2a4a06b0bb75107a1
Evan Diewald <evandiewald@gmail.com>
2023-09-22 13:57:19 -0400

    cleanup

--- bin/cdk-github-actions-sample.ts
+++ bin/cdk-github-actions-sample.ts
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { PipelineStack } from "../lib/pipeline-stack";
-import {PipelineAppStage} from "../lib/pipeline-app-stage";
 import {CdkGithubActionsSampleStack} from "../lib/cdk-github-actions-sample-stack";
 
 
 const app = new cdk.App();
 
-// const devAccountId = process.env.DEVELOPER_ACCOUNT_ID;
-const stack = new CdkGithubActionsSampleStack(app, 'CdkGithubActionsSampleStack', {});
+new CdkGithubActionsSampleStack(app, 'CdkGithubActionsSampleStack', {});
 
 app.synth();
