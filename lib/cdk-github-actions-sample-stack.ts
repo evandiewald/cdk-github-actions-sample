9c6aeea2c0eb92c1a89e233173d7d5b6e8150eb8
Evan Diewald <evandiewald@gmail.com>
2023-09-27 15:35:37 -0400

    test parameter retrieval

--- lib/cdk-github-actions-sample-stack.ts
+++ lib/cdk-github-actions-sample-stack.ts
@@ -1,20 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
+import { lookupStringParameterValue } from "./utils";
 
 export class CdkGithubActionsSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // example resource
-    const queue = new sqs.Queue(this, 'CdkGithubActionsSampleQueue', {
-      visibilityTimeout: cdk.Duration.seconds(240)
+    new sqs.Queue(this, 'CdkGithubActionsSampleQueue', {
+      visibilityTimeout: cdk.Duration.seconds(240),
     });
 
-    const lambdaLayer = new lambda.LayerVersion(this, 'LambdaLayer', {
+    new lambda.LayerVersion(this, 'LambdaLayer', {
       code: lambda.Code.fromAsset(path.join(__dirname, '../runtime/layer.zip'))
+    });
+
+    new cdk.CfnOutput(this, 'ParameterInDeploymentAccount', {
+      value: lookupStringParameterValue('cdk-github-action-role-arn')
+    });
+
+    new cdk.CfnOutput(this, 'ParameterInTargetAccount', {
+      value: ssm.StringParameter.fromStringParameterName(this, 'EnvironmentNameParameter', 'environment-name').stringValue,
     });
   }
 }
